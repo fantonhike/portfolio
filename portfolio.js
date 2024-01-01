3c110bc7d1da15ee8f208a3f457aedaed100d3fb
fantonhike <133677552+fantonhike@users.noreply.github.com>
2024-01-01 14:24:48 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,12 +2,70 @@ $(document).ready(function () {
     var divs = $('.container');
     var dir = 'up'; // wheel scroll direction
     var div = 0; // current div
+    var pointertype = 1;
+    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
+        if (pointertype == 1) {
+        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+            dir = 'down';
+        } else {
+            dir = 'up';
+        }
+        // find currently visible div :
+        div = -1;
+        divs.each(function(i){
+            if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
+                div = i;
+            }
+        });
+        if (dir == 'up' && div > 0) {
+            div--;
+            var value = parseInt(document.getElementById('number').value, 10);
+            value = isNaN(value) ? 0 : value;
+            value--;
+            document.getElementById('number').value = value;
+        }
+        if (dir == 'down' && div < divs.length) {
+            div++;
+            var value = parseInt(document.getElementById('number').value, 10);
+            value = isNaN(value) ? 0 : value;
+            value++;
+            if (value > 9) // this number needs to be equal to the number of pages!!!
+              value = 9;
+            document.getElementById('number').value = value;
+        }
+        //console.log(div, dir, divs.length);
+        $('html,body').stop().animate({
+            scrollTop: divs.eq(div).offset().top
+        }, 0);
+        if (value <= 5) {
+          document.getElementById('pictishcasebutton').setAttribute('style', 'color: white');
+          document.getElementById('alchemistbutton').setAttribute('style', 'color: #707070');
+          document.getElementById('atchoumbutton').setAttribute('style', 'color: #707070');
+        }
+        if (value > 5 && value < 9) {
+          document.getElementById('pictishcasebutton').setAttribute('style', 'color: #707070');
+          document.getElementById('alchemistbutton').setAttribute('style', 'color: white');
+          document.getElementById('atchoumbutton').setAttribute('style', 'color: #707070');
+        }
+        if (value == 9) {
+          document.getElementById('pictishcasebutton').setAttribute('style', 'color: #707070');
+          document.getElementById('alchemistbutton').setAttribute('style', 'color: #707070');
+          document.getElementById('atchoumbutton').setAttribute('style', 'color: white');
+        }
+        return false;
+        }
+    });
+    $(window).resize(function () {
+        if (pointertype == 1) {
+            $('html,body').scrollTop(divs.eq(div).offset().top);
+        }
+    });
 });
 
 document.addEventListener('pointerdown', (event) => {
-  if (event.pointerType === "mouse") {document.getElementById('atchoumbutton').setAttribute('style', 'color: green');}
-  if (event.pointerType === "touch") {document.getElementById('atchoumbutton').setAttribute('style', 'color: red');}
-  if (event.pointerType === "pen") {document.getElementById('atchoumbutton').setAttribute('style', 'color: blue');}
+  if (event.pointerType === "mouse") {pointertype = 1;}
+  if (event.pointerType === "touch") {pointertype = 2;}
+  if (event.pointerType === "pen") {pointertype = 3;}
 });
 
 function pictishcase(){
